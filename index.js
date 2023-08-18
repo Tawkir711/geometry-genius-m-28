d570919eb3f865e3d4a450f25fed1faca722c883
Tawkir711 <mdtawkirk711@gmail.com>
2023-08-18 09:09:49 +0600

    added rectangle

--- index.js
+++ index.js
@@ -10,4 +10,31 @@ function calculateTriangleArea(){
     const hValueText = hField.value;
     const h = parseFloat(hValueText);
     console.log(h);
+
+    const area = 0.5 * b * h;
+    console.log(area) ;
+
+
+    // show triangle area
+    const areaSpan = document.getElementById('triangle-area');
+    areaSpan.innerText = area;
+}
+// step 2 no comment but oporertar moto sme
+
+function calculateRectangleArea(){
+    const wField = document.getElementById('rectangle-w');
+    const wValueText = wField.value;
+    const w = parseFloat(wValueText);
+    console.log(w);
+
+    const lField = document.getElementById('rectangle-l');
+    const lValueText = lField.value;
+    const l = parseFloat(lValueText);
+    console.log(l);
+
+    const area = w * l ;
+
+    const rectangleArea = document.getElementById('rectangle-area');
+    rectangleArea.innerText = area;
+
 }
