5666e1e3ae58fdc1bc7922a6b2793923f9760b78
Tawkir711 <mdtawkirk711@gmail.com>
2023-08-18 17:54:33 +0600

    added parallelogram

--- index.js
+++ index.js
@@ -38,3 +38,30 @@ function calculateRectangleArea(){
     rectangleArea.innerText = area;
 
 }
+
+// reusable function -- reduce duplicate code
+function calculateParallelogramArea(){
+    const base = getInputValue('parallelogram-base');
+    // console.log(base);
+
+    const height = getInputValue('parallelogram-height');
+    // console.log(height);
+
+    const area = base * height;
+    setElementInnerText('parallelogram-area', area);
+    
+}
+
+// reusavle get input value field in number
+function getInputValue(fieldId){
+    const inputField = document.getElementById(fieldId);
+    const inputValueText = inputField.value;
+    const value = parseFloat(inputValueText);
+    return value;
+}
+
+//reusable set span, p, div, etc text
+function setElementInnerText(elementId, area){
+    const element = document.getElementById(elementId)
+    element.innerText = area;
+}
